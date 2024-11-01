a73e00abd1138a1dd4332e03abd30c78a04ad6e3
Andrey Strinski <kroxitrock@gmail.com>
2024-11-01 14:35:51 +0200

    Add api url to environment variable

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,6 +4,6 @@ import User from "../model/user";
 const path = "/users";
 
 export async function getUsers(): Promise<User[]> {
-  const response = await axios.get("http://localhost:3000" + path);
+  const response = await axios.get(process.env.API_URL + path);
   return response.data;
 }
