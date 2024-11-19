617eb2e2b1e9bb3c06725226d3ad9af4d3c9f28c
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-11-19 15:41:46 +0200

    Use split view

--- src/routes/EventDetails.tsx
+++ src/routes/EventDetails.tsx
@@ -1,22 +1,40 @@
 import EventHeader from "@/components/EventHeader";
 import { Button } from "@/components/ui/button";
+import {
+  LeftView,
+  RightView,
+  SplitView,
+  ViewContent,
+  ViewTitle,
+} from "@/components/ui/split_view";
 
 function EventDetails() {
   return (
     <div className="flex flex-col items-center justify-between">
       <EventHeader />
-      {/* <Tabs defaultValue="events" className="w-full md:max-w-4xl">
-        <TabsList>
-          <TabsTrigger value="events">Events</TabsTrigger>
-          <TabsTrigger value="about">About</TabsTrigger>
-        </TabsList>
-        <TabsContent value="events">
-          <EventList />
-        </TabsContent>
-        <TabsContent value="about">
-          <AssociationAbout />
-        </TabsContent>
-      </Tabs> */}
+
+      <SplitView>
+        <LeftView>
+          <ViewTitle>Desciption</ViewTitle>
+          <ViewContent>
+            {`Aalto Salsa Society ry is a vibrant and dynamic student association dedicated to the passion of salsa dancing.
+                Founded at Aalto University, this society has become a thriving community for individuals who love the energy and rhythm of Latin music.
+
+                Our Mission:
+                - To create a welcoming and inclusive environment for salsa enthusiasts of all levels.
+                - To promote the cultural richness and beauty of salsa dancing.
+                - To provide opportunities for members to learn, practice, and perform salsa.
+
+                What We Offer:
+                - Regular Salsa Classes: Learn from experienced instructors and master the fundamentals of salsa dancing.
+                - Practice Sessions: Refine your skills and connect with fellow dancers in a fun and supportive atmosphere.
+                - Social Events: Enjoy salsa parties, socials, and other events to meet new people and celebrate the joy of dancing.`}
+          </ViewContent>
+        </LeftView>
+        <RightView>
+          <ViewTitle>Contacts</ViewTitle>
+        </RightView>
+      </SplitView>
 
       {/* TODO: Fix the place of the button to be on the bottom  */}
       <Button variant="action" className="mb-4">
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,9 @@
 import ComponentsExample from "@/components/ComponentsExample";
 import "./Home.css";
 import EventDetails from "./EventDetails";
-import AssociationDetails from "./AssociationDetails";
 
 function Home() {
-  return <AssociationDetails associationId={0} />;
+  return <EventDetails />;
 }
 
 export default Home;
