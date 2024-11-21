ae5299f872e2a72e8a05038708079392cf326a75
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-11-21 22:24:51 +0200

    Remove images

--- src/components/MembersList.tsx
+++ src/components/MembersList.tsx
@@ -18,32 +18,14 @@ function MemberList() {
           {data?.map((participant, index) => (
             <CategoryItem
               key={participant.id}
-              className={`flex items-center ${
+              className={`flex items-center p-4 ${
                 index % 2 === 0 ? "bg-shadowDark" : "bg-shadowDark-light"
               }`}
             >
-              <CardImage
-                className="w-8 md:w-8 m-2 rounded-full"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZnSqlKhf4aBLhzro-PjB7wX4Eqy2E3jCE8A&s"
-                alt="Aalto Salsa logo"
-              />
               {participant.name}
             </CategoryItem>
           ))}
         </Category>
-        {/* <Category>
-        <CategoryTitle>Waiting</CategoryTitle>
-        {Array.from({ length: 10 }).map((_, index) => (
-          <CategoryItem key={index} className="flex items-center">
-            <CardImage
-              className="w-8 md:w-8 m-2 rounded-full"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZnSqlKhf4aBLhzro-PjB7wX4Eqy2E3jCE8A&s"
-              alt="Aalto Salsa logo"
-            />
-            @aaltosalsaintelegram
-          </CategoryItem>
-        ))}
-      </Category> */}
       </div>
     </div>
   );
