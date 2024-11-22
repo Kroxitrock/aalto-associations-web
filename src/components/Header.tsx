42915fc253929732a6e521b4ffe84699d91dce5c
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-11-23 00:24:41 +0200

    header logo navigates to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { LogIn } from "lucide-react";
 import HeaderAssociationSearch from "./HeaderAssociationSearch";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
   return (
-    // TODO: Add on click on the logo to go to home
     <header className="flex justify-between items-center pb-4">
-      <div className="text-2xl font-bold">A! Associations</div>
+      <div
+        className="text-2xl font-bold cursor-pointer"
+        onClick={() => navigate(`/`)}
+      >
+        A! Associations
+      </div>
 
       <div className="flex items-center gap-4">
         <HeaderAssociationSearch />
