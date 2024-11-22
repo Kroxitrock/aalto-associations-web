23d092504162cf16910a335f0c240c48d4628f65
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-11-22 12:27:37 +0200

    Create event UI

--- src/routes/CreateEvent.tsx
+++ src/routes/CreateEvent.tsx
@@ -22,6 +22,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { CalendarIcon } from "lucide-react";
 import { format } from "date-fns";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import React from "react";
 
 const formSchema = z.object({
   title: z.string().min(1, {
@@ -118,14 +120,13 @@ function Description({ form }: Prop) {
 // TODO: font is different
 // TODO: put time
 function Date({ form }: Prop) {
-  console.log(form);
   return (
     <FormField
       control={form.control}
       name="dob"
       render={({ field }) => (
         <FormItem className="w-1/2 flex flex-col mt-3">
-          <FormLabel>Date of birth</FormLabel>
+          <FormLabel>Pick a date</FormLabel>
           <Popover>
             <PopoverTrigger asChild>
               <FormControl>
@@ -138,9 +139,13 @@ function Date({ form }: Prop) {
                 >
                   <span className="flex-1">
                     {field.value ? (
-                      format(field.value, "PPP")
+                      `${format(field.value, "PPP")} at ${String(
+                        field.value.getHours()
+                      ).padStart(2, "0")}:${String(
+                        field.value.getMinutes()
+                      ).padStart(2, "0")}`
                     ) : (
-                      <span>Pick a date</span>
+                      <span>Pick a date and time</span>
                     )}
                   </span>
                   <CalendarIcon className="ml-2" />
@@ -148,12 +153,64 @@ function Date({ form }: Prop) {
               </FormControl>
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                mode="single"
-                selected={field.value}
-                onSelect={field.onChange}
-                initialFocus
-              />
+              <div className="sm:flex">
+                <Calendar
+                  mode="single"
+                  selected={field.value}
+                  onSelect={field.onChange}
+                  initialFocus
+                />
+                <div className="flex flex-col sm:flex-row sm:h-[300px] divide-y sm:divide-y-0 sm:divide-x">
+                  <ScrollArea className="w-64 sm:w-auto">
+                    <div className="flex sm:flex-col p-2">
+                      {Array.from({ length: 24 }, (_, hour) => (
+                        <Button
+                          key={hour}
+                          variant={
+                            field.value &&
+                            field.value.getHours() % 12 === hour % 12
+                              ? "info"
+                              : "outline"
+                          }
+                          className="sm:w-full shrink-0 aspect-square"
+                          onClick={() => {
+                            field.value?.setHours(hour);
+                            field.onChange(field.value);
+                          }}
+                        >
+                          {hour}
+                        </Button>
+                      ))}
+                    </div>
+                    <ScrollBar orientation="horizontal" className="sm:hidden" />
+                  </ScrollArea>
+                  <ScrollArea className="w-64 sm:w-auto">
+                    <div className="flex sm:flex-col p-2">
+                      {Array.from({ length: 12 }, (_, i) => i * 5).map(
+                        (minute) => (
+                          <Button
+                            key={minute}
+                            variant={
+                              field.value &&
+                              field.value?.getMinutes() === minute
+                                ? "info"
+                                : "outline"
+                            }
+                            className="sm:w-full shrink-0 aspect-square"
+                            onClick={() => {
+                              field.value?.setMinutes(minute);
+                              field.onChange(field.value);
+                            }}
+                          >
+                            {minute}
+                          </Button>
+                        )
+                      )}
+                    </div>
+                    <ScrollBar orientation="horizontal" className="sm:hidden" />
+                  </ScrollArea>
+                </div>
+              </div>
             </PopoverContent>
           </Popover>
           <FormMessage />
