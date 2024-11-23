e0ed73afb2ad3a3f0bb5ba8b44d8662515a273c6
Andrey Strinski <kroxitrock@gmail.com>
2024-11-23 21:22:31 +0200

    Add effective redirect on main page

--- src/routing/routes/Home.tsx
+++ src/routing/routes/Home.tsx
@@ -6,14 +6,18 @@ import { MyAssociationsProvider } from "@/provider/MyAssociationsProvider";
 import { MyEventsProvider } from "@/provider/MyEventProvider";
 import { EventListType } from "@/model/event";
 import useAuthorization from "@/hooks/useAuthorization";
-import Associations from "./Associations";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Home() {
   const { isAuthorized } = useAuthorization();
+  const navigate = useNavigate();
 
-  if (!isAuthorized()) {
-    return <Associations />;
-  }
+  useEffect(() => {
+    if (!isAuthorized()) {
+      navigate("/associations");
+    }
+  }, [isAuthorized, navigate]);
 
   return (
     <div>
